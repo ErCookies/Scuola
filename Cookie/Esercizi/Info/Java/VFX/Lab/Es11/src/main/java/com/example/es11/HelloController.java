bd792a4c0bf2e7242968fc44ba46d54ab83d8411
ErCookies <ercookies3@gmail.com>
2025-01-08 13:23:49 +0100

    update info es 11 lab con gestione errori

--- Cookie/Esercizi/Info/Java/VFX/Lab/Es11/src/main/java/com/example/es11/HelloController.java
+++ Cookie/Esercizi/Info/Java/VFX/Lab/Es11/src/main/java/com/example/es11/HelloController.java
@@ -7,22 +7,29 @@ import javafx.scene.control.TextField;
 
 public class HelloController {
 
-    @FXML
-    private Button btnSomma;
+    @FXML private Button btnSomma;
 
-    @FXML
-    private Label lblResult;
+    @FXML private Label lblResult;
 
-    @FXML
-    private TextField txtX1;
+    @FXML private TextField txtX1;
 
-    @FXML
-    private TextField txtX2;
+    @FXML private TextField txtX2;
+
+    @FXML private Label lblErr;
 
     public void btnSommaOnClick(){
         int x1, x2;
-        x1 = Integer.parseInt(this.txtX1.getText());
-        x2 = Integer.parseInt(this.txtX2.getText());
-        this.lblResult.setText(String.valueOf(x1 + x2));
+            try{
+                x1 = Integer.parseInt(this.txtX1.getText());
+                x2 = Integer.parseInt(this.txtX2.getText());
+                this.lblResult.setText(String.valueOf(x1 + x2));
+            }
+            catch(NumberFormatException exc){
+                this.lblErr.setText("Inserire due numeri");
+            }
+    }
+
+    public void txtOnKeyPress(){
+        this.lblErr.setText("");
     }
 }
