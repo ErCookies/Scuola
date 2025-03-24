a04655b29a1e732fca19a3deb9e44c29df0a9b67
ErCookies <ercookies3@gmail.com>
2025-03-24 17:07:38 +0100

    inizio sito

---
+++ Sito_Viaggio_Istruzione/JS/script.js
@@ -0,0 +1,9 @@
+function formOnSubmit(form){
+    const name = form.elements.name.value,
+        sur = form.elements.sur.value,
+        mail = form.elements.mail.value,
+        oggetto = form.elements.oggetto.value,
+        mex = form.elements.mex.value;
+
+    //
+}
